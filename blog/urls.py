986c5c0de2dc30c660f5ee13d45fbec8f6a327d6
sejun2 <qpfjf56@gmail.com>
2022-07-06 17:27:34 +0900

    feat: CBV로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 from blog import views # 현재 폴더에 있는 views.py 을 사용할 수 있게 import
 
 urlpatterns = [
-    path('/', views.index),
-    path('/<int:pk>', views.single_post_page),
-
+    # path('/', views.index),
+    path('/', views.PostList.as_view()),
+    # path('/<int:pk>', views.single_post_page),
+    path('/<int:pk>', views.PostDetail.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,35 +1,47 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
 
 # Create your views here.
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')  # -를 붙이면 역순표시
-
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        },
-    )
+class PostList(ListView):
+    model = Post
+    # template_name = 'blog/index.html'
+    ordering = '-pk'
 
 
-def single_post_page(request, pk):
-    try:
-        post = Post.objects.get(pk=pk)
+class PostDetail(DetailView):
+    model = Post
 
-        return render(
-            request,
-            'blog/single_post_page.html',
-            {
-                'post': post,
-            }
-        )
-
-    except BaseException as e:
-        return render(
-            request,
-            'blog/not_found.html'
-        )
+    ######## FBV ##########
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')  # -를 붙이면 역순표시
+#
+#     return render(
+#         request,
+#         'blog/index.html',
+#         {
+#             'posts': posts,
+#         },
+#     )
+#
+#
+# def single_post_page(request, pk):
+#     try:
+#         post = Post.objects.get(pk=pk)
+#
+#         return render(
+#             request,
+#             'blog/post_detail.html',
+#             {
+#                 'post': post,
+#             }
+#         )
+#
+#     except Exception as e:
+#         return render(
+#             request,
+#             'blog/not_found.html'
+#         )
